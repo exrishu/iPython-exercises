97593b2c16d03eab16c934b7dfc6326d893ca3a1
Ayush Pandey <exrishu123@gmail.com>
2020-10-08 12:22:11 +0530

    removing database original cred

--- Flask-API/app/utils/database_connection.py
+++ Flask-API/app/utils/database_connection.py
@@ -5,7 +5,7 @@ import psycopg2
 class DatabaseConnection:
 	def __init__(self):
 		self.connection = None
-		self.dsn = "dbname=da609iukq3i5lj user=ukzbxlqpyruezw password=71c453ee142b50195819d4b8df51c9bf4b8e1bc61c19e826a1a98502384694a8 host=ec2-3-224-97-209.compute-1.amazonaws.com port=5432"
+		self.dsn = "dbname=databse_name user=user_id password=db_passwrd host=host port=5432"
 
 
 	def __enter__(self):
